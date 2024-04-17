4a08e0d215d5503edcb416747aaa43ed2c9e5c36
biancaBezerra <bianca.skills@gmail.com>
2024-04-17 12:22:32 -0300

    T005: refactor - ajustes no código para retornar o objeto taxi no endpoint de trajectories

--- fleet_management_app/serializers.py
+++ fleet_management_app/serializers.py
@@ -1,13 +1,11 @@
 from rest_framework import serializers
-
 from .models import Taxis, Trajectories
-
 class TaxisSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Taxis
-    fields = '__all__'
-
+    class Meta:
+        model = Taxis
+        fields = ['id', 'plate']  # Inclua todas as informações do táxi que você deseja retornar
 class TrajectoriesSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Trajectories
-    fields = '__all__'
+    taxi = TaxisSerializer()  # Use o serializer do táxi para serializar o objeto de táxi
+    class Meta:
+        model = Trajectories
+        fields = '__all__'
