2aad4e88549ba1830ed81a2548ce8134c7d6a2f8
biancaBezerra <bianca.skills@gmail.com>
2024-05-13 10:24:21 -0300

    T008 - test: refatoração do request de get taxis

--- fleet_management_app/test/test_get_taxis.py
+++ fleet_management_app/test/test_get_taxis.py
@@ -18,7 +18,7 @@ def test_list_taxis_endpoint(mock_taxis_queryset):
     factory = APIRequestFactory()
 
     # Making a GET request to the endpoint
-    request = factory.get('http://localhost:8000/api/taxis/')
+    request = factory.get('/api/taxis/')
     response = get_taxis(request)
 
     # Asserting that the list of taxis is not empty
@@ -29,7 +29,7 @@ def test_list_taxis_filter(mock_taxis_queryset):
     factory = APIRequestFactory()
 
     # Making a GET request to the endpoint with filter parameter
-    request = factory.get('http://localhost:8000/api/taxis/', {'id': 1})
+    request = factory.get('/api/taxis/', {'id': 1})
     response = get_taxis(request)
 
     # Asserting the response data
@@ -41,7 +41,7 @@ def test_list_taxis_sort(mock_taxis_queryset):
     factory = APIRequestFactory()
 
     # Making a GET request to the endpoint with sort parameter
-    request = factory.get('http://localhost:8000/api/taxis/', {'order_by': '-id'})
+    request = factory.get('/api/taxis/', {'order_by': '-id'})
     response = get_taxis(request)
 
     # Asserting the response data
@@ -54,7 +54,7 @@ def test_list_taxis_search(mock_taxis_queryset):
     factory = APIRequestFactory()
 
     # Making a GET request to the endpoint with search parameter
-    request = factory.get('http://localhost:8000/api/taxis/', {'id': 2})
+    request = factory.get('/api/taxis/', {'id': 2})
     response = get_taxis(request)
 
     # Asserting the response status code
@@ -64,13 +64,13 @@ def test_list_taxis_search(mock_taxis_queryset):
     assert len(response.data['results']) == 2
     assert response.data['results'][1]['id'] == 2
 
-def test_list_taxis_pagination(mock_taxis_queryset):
+def test_list_taxis_page_size(mock_taxis_queryset):
     mock_taxis = [Taxis(id=i, plate=f"ABC-{i}") for i in range(1, 21)]  # foram criados 20 taxis mock
     mock_taxis_queryset.return_value = mock_taxis
 
     factory = APIRequestFactory()
 
-    request = factory.get('http://localhost:8000/api/taxis/', {'page_size': 1})
+    request = factory.get('/api/taxis/', {'page_size': 1})
     response = get_taxis(request)
 
     # Asserting the response status code
@@ -82,3 +82,22 @@ def test_list_taxis_pagination(mock_taxis_queryset):
     assert response.data['total_pages'] == 20
     assert response.data['next'] is not None
     assert response.data['previous'] is None
+
+def test_list_taxis_pagination(mock_taxis_queryset):
+    mock_taxis = [Taxis(id=i, plate=f"ABC-{i}") for i in range(1, 21)]  # foram criados 20 taxis mock
+    mock_taxis_queryset.return_value = mock_taxis
+
+    factory = APIRequestFactory()
+
+    request = factory.get('/api/taxis/', {'page': 2})
+    response = get_taxis(request)
+
+    # Asserting the response status code
+    assert response.status_code == 200
+
+    # Asserting the response data
+    assert len(response.data['results']) == 10  # Aqui assumimos que será retornado 10 taxis por pagina
+    assert response.data['current_page'] == 2
+    assert response.data['total_pages'] == 2
+    assert response.data['next'] is None
+    assert response.data['previous'] is not None
